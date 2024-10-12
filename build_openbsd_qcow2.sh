44d0791d0de94db900e2f7ccc13260985aa2f3f5
Matthews24 <99995715+Mateus2022@users.noreply.github.com>
2024-10-12 16:34:27 -0300

    Update build_openbsd_qcow2.sh

--- build_openbsd_qcow2.sh
+++ build_openbsd_qcow2.sh
@@ -32,7 +32,7 @@ PATH_IMAGES="${TOP_DIR}/images"
 PATH_TFTP="${TOP_DIR}/tftp"
 
 OPENBSD_VERSION="7.6"
-OPENBSD_ARCH=amd64
+OPENBSD_ARCH=arm64
 
 OPENBSD_TRUSTED_MIRROR_BASE="https://ftp.openbsd.org/pub/OpenBSD"
 OPENBSD_MIRROR_BASE="https://cdn.openbsd.org/pub/OpenBSD"
